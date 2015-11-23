91f8336e5524233d71f5f1c69f681142177c3561
Wesley Dunn <wmdunn1020@gmail.com>
2015-11-23 16:48:28 -0600

    Commit test to anotherBranch

--- dictionaryTesting/dictionaryTesting/Program.cs
+++ dictionaryTesting/dictionaryTesting/Program.cs
@@ -10,6 +10,12 @@ namespace dictionaryTesting
     {
         static void Main(string[] args)
         {
+            var map = new Dictionary<string,int>();
+            map.Add("foo",10434234);
+            map["bar"] = 23452;
+            var value = map["foo"];
+            Console.WriteLine(value);
+            Console.ReadKey();
         }
     }
 }
