9c8e6d1c1ebea0712de4ee1204631afc60d06228
Wesley Dunn <wmdunn1020@gmail.com>
2015-11-23 16:40:37 -0600

    DictionaryTesting

--- listTesting1/listTesting1/Program.cs
+++ listTesting1/listTesting1/Program.cs
@@ -10,7 +10,7 @@ namespace listTesting1
     {
         static void Main(string[] args)
         {
-            List<string> names = new List<string>();
+            var names = new List<string>();
             names.Add("blah");
             names.Add("yah");
             foreach (string name in names)
