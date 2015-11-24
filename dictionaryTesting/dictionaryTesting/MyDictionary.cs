1ef7b87c1791bb6e853d7a739238bf9b39eaa9b2
Wesley Dunn <wmdunn1020@gmail.com>
2015-11-24 09:19:15 -0600

    Added myDictionary class

---
+++ dictionaryTesting/dictionaryTesting/MyDictionary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Services;
+
+namespace dictionaryTesting
+{
+    public class MyDictionary
+    {
+        Dictionary<string,int> map = new Dictionary<string, int>(); 
+
+        public MyDictionary(int valueCount)
+        {
+            for (int i = 0; i < valueCount; i++)
+            {
+                Console.WriteLine("Enter a key and value(int), delimited with a space: ");
+                string temp = Console.ReadLine();
+                string[] parsedString = temp.Split(' ');             
+                map.Add(parsedString[0], Convert.ToInt32(parsedString[1]));
+            }
+        }
+
+        public void printDictionaryWithKey(string key)
+        {
+            int value;
+            map.TryGetValue(key, out value);
+            Console.WriteLine(value);
+        }
+          
+    }
+}
--- dictionaryTesting/dictionaryTesting/Program.cs
+++ dictionaryTesting/dictionaryTesting/Program.cs
@@ -10,11 +10,8 @@ namespace dictionaryTesting
     {
         static void Main(string[] args)
         {
-            var map = new Dictionary<string,int>();
-            map.Add("foo",10434234);
-            map["bar"] = 23452;
-            var value = map["foo"];
-            Console.WriteLine(value);
+           MyDictionary aDictionary = new MyDictionary(2);
+            aDictionary.printDictionaryWithKey("foo");
             Console.ReadKey();
         }
     }
